f29c1dc54705cd3090bc4d39d5ddda508603c3d7
Daran Dachte <darandachte@gmail.com>
2023-01-08 17:58:51 +0100

    clouds animation done

--- src/components/Cloud/Cloud.jsx
+++ src/components/Cloud/Cloud.jsx
@@ -6,48 +6,50 @@ const Cloud = ({ text, url }) => {
       <NavLink to={url} className={styles.text}>
         {text}
       </NavLink>
-      <div className={`${styles.cloud} ${styles.cloudBase}`}></div>
-      <div className={`${styles.cloud} ${styles.cloudBack}`}></div>
-      <div className={`${styles.cloud} ${styles.cloudMid}`}></div>
-      {/* <div className={`${styles.cloud} ${styles.cloudFront}`}></div> */}
-      <svg width="0" height="0">
-        <filter id="filter-base">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.011"
-            numOctaves="5"
-            seed="8517"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="120" />
-        </filter>
-        <filter id="filter-back">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.011"
-            numOctaves="3"
-            seed="8517"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="120" />
-        </filter>
-        <filter id="filter-mid">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.011"
-            numOctaves="3"
-            seed="8517"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="120" />
-        </filter>
-        <filter id="filter-front">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency="0.009"
-            numOctaves="4"
-            seed="8517"
-          />
-          <feDisplacementMap in="SourceGraphic" scale="50" />
-        </filter>
-      </svg>
+      <div className={styles.cloudAnimation}>
+        <div className={`${styles.cloud} ${styles.cloudBase}`}></div>
+        <div className={`${styles.cloud} ${styles.cloudBack}`}></div>
+        <div className={`${styles.cloud} ${styles.cloudMid}`}></div>
+        {/* <div className={`${styles.cloud} ${styles.cloudFront}`}></div> */}
+        <svg width="0" height="0">
+          <filter id="filter-base">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.011"
+              numOctaves="5"
+              seed="8517"
+            />
+            <feDisplacementMap in="SourceGraphic" scale="120" />
+          </filter>
+          <filter id="filter-back">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.011"
+              numOctaves="3"
+              seed="8517"
+            />
+            <feDisplacementMap in="SourceGraphic" scale="120" />
+          </filter>
+          <filter id="filter-mid">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.011"
+              numOctaves="3"
+              seed="8517"
+            />
+            <feDisplacementMap in="SourceGraphic" scale="120" />
+          </filter>
+          <filter id="filter-front">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.009"
+              numOctaves="4"
+              seed="8517"
+            />
+            <feDisplacementMap in="SourceGraphic" scale="50" />
+          </filter>
+        </svg>
+      </div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,24 @@ import Cloud from "../components/Cloud";
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}> 
-      <Cloud text="About" url="/about" />
+    <div className={styles.wrapper}>
+      <div className={styles.cloudList}>
+        <div className={styles.position}>
+          <Cloud text="About" url="/about" />
+        </div>
+        <div className={styles.position}>
+          <Cloud text="Diary" url="/diary" />
+        </div>
+        <div className={styles.position}>
+          <Cloud text="Choose your Rain" url="/Choose_your_Rain" />
+        </div>
+        <div className={styles.position}>
+          <Cloud text="Find your Rain" url="/Find_your_Rain" />
+        </div>
+        <div className={styles.position}>
+          <Cloud text="This Rain is Classic" url="/This_Rain_is_Classic" />
+        </div>
+      </div>
       {/* <video src={video} autoPlay muted loop id="myVideo"></video> */}
     </div>
   );
