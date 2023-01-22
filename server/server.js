348751b0f35130e790012ee24bf144734dac8be9
Daran Dachte <darandachte@gmail.com>
2023-01-22 22:53:19 +0100

    server was added

--- server/server.js
+++ server/server.js
@@ -1,29 +1,68 @@
-import express from "express";
+import express, { request, Router } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
-import addRoutes from "./routes/addRoutes.js";
+import cors from "cors";
+const { Schema, model } = mongoose;
 
 dotenv.config();
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+mongoose.set("strictQuery", true);
 
-const PORT = process.env.PORT || 3000;
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    console.log("Database connected! 😃");
+    console.log("we are connected to the database.");
   })
   .catch((error) => {
-    console.log(error.message);
-    console.log("🤨");
+    console.log("an error occurred while connecting ot the db", error);
   });
 
-app.use("/api/add", addRoutes);
-
-app.listen(PORT, (req, res) => {
-  console.log(`Server is listening for HTTP requests on port ${PORT}`);
+app.listen(process.env.PORT || 3001, (req, res) => {
+  console.log(`the server listening on port ${PORT}`);
 });
+
+// Schema
+const questionsSchema = new Schema({
+  topic: {
+    type: String,
+    required: true,
+    enum: {
+      values: [
+        "I have depression",
+        "Question for a lawyer",
+        "Social help",
+        "Work in Germany",
+        "Education",
+        "Medicine and health",
+        "Violence in family",
+        "Other",
+      ],
+    },
+  },
+  name: { type: String, required: true },
+  text: { type: String, required: true },
+});
+
+// Model
+const Question = model("Question", questionsSchema);
+
+// Routing
+const router = Router();
+router.post("/questions", async (req, res) => {
+  const { topic, name, text } = req.body;
+  try {
+    const question = await Question.create({ topic, name, text });
+    res.json(question);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+});
+
+app.use("/", router);
