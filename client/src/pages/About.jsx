48c7d11f560f77857a3a2f0505cae2be5a71ad90
Daran Dachte <darandachte@gmail.com>
2023-01-25 17:27:35 +0100

    long explanation was added

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -124,50 +124,31 @@ const About = () => {
             isActive={version === 2}
             setActiveVersion={setActiveVersion}
             title="Long Explanation"
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. At,
-                dignissimos ex corporis, quidem architecto itaque perferendis
-                suscipit voluptas quibusdam ipsum veniam autem, quod maxime quo
-                non adipisci qui nisi laudantium impedit quisquam neque iste
-                nulla ea ullam! Animi, fugiat delectus libero quia at,
-                consectetur fuga ab voluptatum aliquam illum totam aspernatur
-                sunt quas ducimus mollitia dolore dignissimos voluptate expedita
-                ipsam quam beatae exercitationem. Molestiae tempora laboriosam,
-                nulla cum ad magnam temporibus reprehenderit excepturi quos
-                perferendis, amet perspiciatis accusantium deserunt adipisci
-                dolore assumenda sunt autem laudantium voluptate aperiam eum.
-                Quod placeat iusto tenetur est pariatur eum quidem vitae non
-                temporibus veniam amet doloribus at ipsum, esse velit eos saepe
-                ratione, voluptatibus incidunt sit id? Velit nulla aspernatur
-                facere laudantium illum cum, ut possimus earum accusantium odio
-                nobis eum unde aperiam dolor nihil? Neque expedita dolor laborum
-                qui optio delectus, doloribus ipsam! Ipsa molestias, repellendus
-                eum libero necessitatibus quia veritatis nulla corrupti tempore
-                accusamus expedita laudantium quaerat mollitia enim minima
-                provident odit deleniti fuga error! Repudiandae cum, deserunt
-                dolorum blanditiis repellendus obcaecati iure facere! Omnis
-                perferendis repellat soluta laboriosam voluptatem reiciendis
-                odio facere doloremque delectus vero veniam perspiciatis debitis
-                aut veritatis, earum exercitationem! Deserunt cupiditate quia
-                exercitationem. Voluptatum explicabo, asperiores animi quo
-                praesentium quaerat. Asperiores magni quod sit neque consectetur
-                pariatur odit, aliquam blanditiis sapiente officia non libero
-                mollitia quo recusandae eaque eum assumenda, eos similique iste
-                cumque amet? Maiores at obcaecati, laudantium nihil corporis
-                praesentium necessitatibus quidem nisi ipsam eveniet! Voluptate
-                libero vitae consequuntur earum quas doloremque dignissimos
-                laborum? Quaerat tenetur ad autem quia blanditiis ipsum.
-                Corrupti, voluptates ducimus. Modi voluptatum quis, sint velit
-                saepe, veniam eius enim tempora sapiente reprehenderit vel ea
-                provident at quam blanditiis aliquam quibusdam reiciendis
-                nesciunt optio repellendus temporibus suscipit ex! Suscipit qui
-                impedit sint quae eius earum ut odio magnam nulla, iste quidem
-                est temporibus adipisci rerum blanditiis minima, similique,
-                voluptate itaque recusandae possimus exercitationem ab aut?
-                Corporis, vitae! Cumque voluptates dolorum aspernatur tempora
-                omnis consequuntur expedita amet sunt quis, earum corporis quas
-                magnam, alias nostrum rem sit aut cum consequatur est esse? At
-                excepturi, molestias nostrum id, culpa repellat voluptates ullam
-                obcaecati voluptatem perferendis similique aspernatur quis"
+            text="Hello, my name is Daran Daсhte. It so happens that I have been a refugee twice in my life. The first time was when I was still a teenager. My family fled the war from the outskirts of my homeland, where we were living at the time, to the capital. It was then that I learned for the first time what refugee life was like. You are uprooted from your familiar surroundings and placed in a new environment where everything is foreign to you. Everything makes you sick - different climate, different food, different emotions. You have to understand the world around you all over again and learn to live.
+
+            The second time I became a refugee was when I was almost forty. It was because of my involvement in a protest against my government's policies. I was detained by the police many times, then I ended up behind bars twice, fortunately for a short time. Then the moment came when I had to leave my homeland and become a refugee again. 
+            The second time was somewhat easy for me. I was an adult, and I knew that I would have to fight a lot, and no one would guarantee me victory. It strips you of unnecessary illusions. I knew that this condition - being a refugee - requires you to do extraordinary things, it requires you to concentrate all your will, all your feelings, in an attempt to survive and preserve yourself, or better yet, to create a comfortable environment around you, where you will be surrounded by decent people, where you will feel needed and useful to society, where you can breathe peacefully during the day and sleep soundly at night. 
+            
+            Being a refugee is always difficult. You find yourself in a new place where, frankly, you are not welcome. Even if it is your homeland. And if it is a new country where you do not know anyone, where they speak a foreign language... it becomes the test of your life where your every ability will be tested.
+            
+           Yes, in the beginning you feel good because you find yourself in a safe place. But then there is a period when every day you have to find the strength to be stronger than the circumstances. You have to learn the language, customs, very often you have to learn a new profession. It is always hard, it is always demanding. 
+            I have not seen a single refugee who has not gone through depression and despair. I don't know a single one who has started his life with nothing but joy and happiness.
+            In this daily struggle, you make mistakes that can be fatal. Sometimes these mistakes can even lead to your deportation from this country, and very often it means the end of all your hopes for a better life.
+            I have been in contact with hundreds of people who have moved to Germany. I helped them as a translator, as an advisor - where to go, who to talk to. I tried to give them the confidence that they could win the struggle. Some of them succeeded and are now living a happy life. Some broke down, achieved nothing and were deported.
+After a while I realized that many refugees make the same mistakes. There are not many of them, they can be reduced to a few main ones. And the most important one, which is very common, looks like this:
+'Many refugees have plans to reach Germany(Italy, France....). But they have no plans for what they will do in that Germany'. 
+This may sound ridiculous to some, but it's true. I knew a man who took 4(!) years to get from Eritrea to Westphalia. He managed to go to prison, he was robbed, he almost died several times... But when he got there, where he had been going for so long, he found out that that was the end of it. He had no idea what to do next.That is, he had been programmed to stop fighting at the very moment when the fight was due to begin.
+Reaching another country was the end - that was it, I wanted to come, I arrived, the programme was over. And when everything is just beginning, when there are chances to go to school, to learn the language and then to get an education and a job - many of these people have no idea what they want, in which direction they want to go. Although many of them are talented, many have decent potential to discover themselves.
+
+For Germany, this represents a loss of billions. The country is full of hostels for refugees who, after a short period, become disillusioned with everything. They lose their old illusions and gain nothing in return. They have no purpose, they don't know where they could be useful.  And even if they later get a chance to study and work, a state of depression (that is, they are functioning at twenty percent of their capacity) stays with them for the rest of their lives. And later it is inherited by their children, for whom it becomes the norm of behaviour. 
+
+There is such an expression - the American Dream. It means that people are sure that they can come to America, be poor and strangers and after a while become rich and belong to them, in this country-the USA.It is clear that in many respects this is an imposed myth. There are enough problems in the USA, expensive education and medicine. In Germany I did not pay my dentist a single euro in the first seven years of my life. An education that cost 30,000 euros, I got for free. So Germany has everything you need to get the title of 'German Dream' or 'Germany is a country of success'. But you have to work at it, like they did in America, the myth of the American Dream did not arise by itself.
+Germany is a country where your dreams for a decent life can be fulfilled, and it has everything for that. You just have to allocate your energy properly and be honest with yourself and those around you. To discover yourself, to understand what you really have, what hidden talents you have in you, the ones you did not know about in your homeland, because your homeland at this moment in time is at another level of social development. 
+Modern Germany has developed as a country of newcomers - refugees, guest workers, migrants... Immediately after the war, the newcomers were Germans themselves - from the Sudetes, Prussia, Oberschlesia..... Then there were Turks, Arabs, Albanians, Germans again, but from the post-Soviet space, Jews, Africans and Ukrainians This is likely to continue. Unless something extraordinary happens.
+
+All of the above explains why I decided to do this project. It is something in between a social and cultural project. On the one hand it should help those who come to Germany. On the one hand, it should help people who came to Germany to adapt, discover their potential and use it to their benefit and their new country of residence. On the other hand, it is a cultural project. I would like to start helping first of all with visual methods. That is, the website should not be a dry set of information and advice. 'Do this, go there, call this number...' A website should be, first and foremost, a place where people know at a glance what it is all about, that they can be helped here. It is trustworthy and welcoming. 
+The site should contain positive examples from refugees' lives. These examples can be a landmark, a beacon in the direction of which one should move to overcome problems. There are a lot of negative moments in places of compact residence of refugees - someone sells drugs, someone uses them, someone steals... All this creates a huge negative field, which absorbs new people and from which it can be very difficult to get out.
+            "
           />
         </div>
       </div>
