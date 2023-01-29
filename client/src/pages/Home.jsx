29b21829492ba1f6e0fdf51ae30cf67596c45974
Daran Dachte <darandachte@gmail.com>
2023-01-29 15:48:25 +0100

    home background video was added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,28 +2,31 @@ import React from "react";
 // import video from "../img/rainbow.mp4";
 import styles from "./Home.module.css";
 import Cloud from "../components/Cloud";
-
+import rainbow from "../assets/videos/rainbow.mp4";
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.cloudList}>
-        <div className={styles.position}>
-          <Cloud text="About" url="/about" />
+    <>
+      <video className={styles.video} src={rainbow} autoPlay muted loop></video>
+      <div className={styles.wrapper}>
+        <div className={styles.cloudList}>
+          <div className={styles.position}>
+            <Cloud text="About" url="/about" />
+          </div>
+          <div className={styles.position}>
+            <Cloud text="Diary" url="/diary" />
+          </div>
+          <div className={styles.position}>
+            <Cloud text="Tests News Donations" url="/tests_news_donations" />
+          </div>
+          <div className={styles.position}>
+            <Cloud text="Questions and Answers" url="/questions_and_answers" />
+          </div>
+          <div className={styles.position}>
+            <Cloud text="Gallery" url="/gallery" />
+          </div>
         </div>
-        <div className={styles.position}>
-          <Cloud text="Diary" url="/diary" />
-        </div>
-        <div className={styles.position}>
-          <Cloud text="Tests News Donations" url="/tests_news_donations" />
-        </div>
-        <div className={styles.position}>
-          <Cloud text="Questions and Answers" url="/questions_and_answers" />
-        </div>
-        <div className={styles.position}>
-          <Cloud text="Gallery" url="/gallery" />
-        </div>
+        {/* <video src={video} autoPlay muted loop id="myVideo"></video> */}
       </div>
-      {/* <video src={video} autoPlay muted loop id="myVideo"></video> */}
-    </div>
+    </>
   );
 }
