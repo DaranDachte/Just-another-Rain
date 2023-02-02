61e46ad967f4afb8a3cc1dff013b5b39d8117eee
Daran Dachte <darandachte@gmail.com>
2023-02-02 22:42:54 +0100

    some styles

--- client/src/components/TestList/TestList.jsx
+++ client/src/components/TestList/TestList.jsx
@@ -2,55 +2,60 @@ import TestItem from "../TestItem";
 import styles from "./TestList.module.css";
 const TestList = () => {
   return (
-    <div className={styles.container}>
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-      <TestItem
-        description="sdfsdfsdfsdf"
-        title="sdfgfsfsdfsdf"
-        link="asdfsdfsdfsdfds"
-      />
-   
-    
-    </div>
+    <>
+      <div className={styles.title}>
+        <h2>Depression Tests</h2>
+        <p>Warning! If you find any signs of depression after taking these tests, copy and Google this phrase. It means finding psychological help near where you live.</p>
+        <p>Hilfe von einem Psychologen in meiner Nähe</p>
+      </div>
+      <div className={styles.container}>
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+        <TestItem
+          description="sdfsdfsdfsdf"
+          title="sdfgfsfsdfsdf"
+          link="asdfsdfsdfsdfds"
+        />
+      </div>
+    </>
   );
 };
 
