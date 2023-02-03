59a0edc326a704426a2770f4c79c5aab041b844f
Daran Dachte <darandachte@gmail.com>
2023-02-03 23:33:43 +0100

    styling of news page

--- client/src/components/NewsItem/NewsItem.jsx
+++ client/src/components/NewsItem/NewsItem.jsx
@@ -3,9 +3,14 @@ import styles from "./NewsItem.module.css";
 const NewsItem = ({ country, language, description, link }) => {
   return (
     <a className={styles.wrapper} href={link} target="_blank" rel="noreferrer">
-      <h3>Origin of news: {country}</h3>
-      <h4>Language of news: {language}</h4>
-      <h5>Short description: "{description}"</h5>
+      <h3>
+        {" "}
+        <em>Origin of news:</em> {country}
+      </h3>
+      <h5>
+        <em>Language of news:</em> {language}
+      </h5>
+      <p className={styles.description}> {description}</p>
     </a>
   );
 };
--- client/src/components/NewsList/NewsList.jsx
+++ client/src/components/NewsList/NewsList.jsx
@@ -11,46 +11,34 @@ const NewsList = () => {
 
       <div className={styles.container}>
         <NewsItem
-          country="France"
-          language="english"
-          description="'Spider-Man' of Paris to get French citizenship after child rescue"
+          country="France 🇫🇷️"
+          language="english 🏴󠁧󠁢󠁥󠁮󠁧󠁿️"
+          description="'Spider-Man' of Paris to get French citizenship after child rescue."
           link="http://https://www.theguardian.com/world/2018/may/28/spider-man-of-paris-to-get-french-citizenship-after-rescuing-child"
         />
         <NewsItem
-          country="Germany"
-          language="german"
-          description="Angolan refugee stopped a burning bus on the A2 motorway and saved the lives of 66 people"
+          country="Germany 🇩🇪️"
+          language="german 🇩🇪"
+          description="Angolan refugee stopped a burning bus on the A2 motorway and saved the lives of 66 people."
           link="https://dorsten-online.de/andre-tiago-manuel-ist-schutzengel-auf-der-a2/"
         />
         <NewsItem
-          country="Russia"
-          language="russian"
-          description="A migrant from Tajikistan saved a child from dying in the Moscow subway"
+          country="Russia 🇷🇺️"
+          language="russian 🇷🇺️"
+          description="A migrant from Tajikistan saved a child from dying in the Moscow subway."
           link="https://bomdodrus.com/2022/03/15/migrant-iz-tadzhikistana-spas-rebenka-ot-smerti-v-moskovskom-metro/"
         />
         <NewsItem
-          country="Russia"
-          language="russian"
-          description="A migrant from Tajikistan saved a child from dying in the Moscow subway"
-          link="https://bomdodrus.com/2022/03/15/migrant-iz-tadzhikistana-spas-rebenka-ot-smerti-v-moskovskom-metro/"
+          country="Norway 🇳🇴️"
+          language="Ukrainian 🇺🇦️"
+          description="A 17-year-old Ukrainian refugee learned the Norwegian language in record time."
+          link="https://life.pravda.com.ua/society/2023/02/1/252648/"
         />
         <NewsItem
-          country="Russia"
-          language="russian"
-          description="A migrant from Tajikistan saved a child from dying in the Moscow subway"
-          link="https://bomdodrus.com/2022/03/15/migrant-iz-tadzhikistana-spas-rebenka-ot-smerti-v-moskovskom-metro/"
-        />
-        <NewsItem
-          country="Russia"
-          language="russian"
-          description="A migrant from Tajikistan saved a child from dying in the Moscow subway"
-          link="https://bomdodrus.com/2022/03/15/migrant-iz-tadzhikistana-spas-rebenka-ot-smerti-v-moskovskom-metro/"
-        />
-        <NewsItem
-          country="Russia"
-          language="russian"
-          description="A migrant from Tajikistan saved a child from dying in the Moscow subway"
-          link="https://bomdodrus.com/2022/03/15/migrant-iz-tadzhikistana-spas-rebenka-ot-smerti-v-moskovskom-metro/"
+          country="France 🇫🇷️"
+          language="german 🇩🇪"
+          description="Lassana Bathily saved several lives in the supermarket attack."
+          link="https://www.faz.net/aktuell/politik/ausland/europa/lassana-bathily-frankreichs-neuer-held-13364961.html"
         />
       </div>
     </>
